9fa66735c02e7d919b4b6dcd27e89fbed44afb35
Gilles <104164728+GillesCob@users.noreply.github.com>
2025-04-08 11:53:17 +0200

    Initial Commit

---
+++ my-app/src/components/HelloComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Coucou toi, Vue!</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloComponent', // Nom du composant
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
