dd46277f750b14abb5a6f2dbc22f88cba62d8c85
Gilles <contact@gillescobigo.com>
2025-07-15 19:43:50 +0200

    Ajout elts pour la connexion MongoDB depuis Render

--- backend/mongo.js
+++ backend/mongo.js
@@ -1,15 +1,26 @@
 import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URL;
-const client = new MongoClient(uri);
+
+const client = new MongoClient(uri, {
+  ssl: true,
+  tlsAllowInvalidCertificates: true, // nécessaire pour le déploiement depuis Render
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 let db;
 
 export async function connect() {
   if (!db) {
-    await client.connect();
-    db = client.db(process.env.MONGODB_DB_NAME);
-    console.log('Connecté à MongoDB');
+    try {
+      await client.connect();
+      db = client.db(process.env.MONGODB_DB_NAME);
+      console.log('✅ Connecté à MongoDB');
+    } catch (err) {
+      console.error('❌ Erreur de connexion MongoDB :', err.message);
+      throw err;
+    }
   }
   return db;
 }
